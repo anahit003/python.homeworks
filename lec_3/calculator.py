240e6d39e2a5c5973f81312084a8a924f5bd0769
Anahit <anahit.xnkanosyan@mail.ru>
2023-10-28 00:46:02 +0400

    lecture3

---
+++ lec_3/calculator.py
@@ -0,0 +1,44 @@
+#calculator
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
+
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+   choice = input("Enter (1 or 2 or 3 or 4): ")
+   if choice in ('1', '2', '3', '4'):
+        
+           num1 = float(input("number1: "))
+           num2 = float(input("number2: "))
+        
+
+           if choice == '1':
+             print(num1, "+", num2, "=", add(num1, num2))
+
+           elif choice == '2':
+             print(num1, "-", num2, "=", subtract(num1, num2))
+
+           elif choice == '3':
+             print(num1, "*", num2, "=", multiply(num1, num2))
+
+           elif choice == '4':
+             print(num1, "/", num2, "=", divide(num1, num2))
+        
+           next_calculation = input(" next calculation? yes|no: ")
+           if next_calculation == "no":
+            break
+   else:
+         print("sxal input")
+
